be433f0a1454fd2fa6e390a75461d69a7560b012
tinogomes <tinorj@gmail.com>
2013-02-28 14:14:23 -0300

    Teste do 35 ok

--- lib/numeros.rb
+++ lib/numeros.rb
@@ -1,12 +1,12 @@
 class Integer
   def prime?
-    bool = indivisivel_por(2) 
-    bool &&= indivisivel_por(3) 
-    if self > 5
-      bool &&= indivisivel_por(5)
-    else
-      return bool
+    bool = true
+    self.times do |i|
+      if i > 1
+        bool &&= indivisivel_por(i)
+      end
     end
+    bool
   end
 
   private
